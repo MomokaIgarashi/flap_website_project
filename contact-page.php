b2119570d609bbee3477eb663e90543611f656c6
MomokaIgarashi <migarashi0318@gmail.com>
2022-02-13 16:33:49 +0100

    contact form set up basic

--- contact-page.php
+++ contact-page.php
@@ -1,60 +1,86 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package FLAP
- */
-
-get_header();
+/*
+Template Name: お問い合わせ
+*/
 ?>
+<?php get_header(); ?>
 
-	<main id="primary" class="site-main">
+<div class="contact-form-wrapper">
+	<div class="contact-form__inner">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact/contact-title-text.png" class="contact__image-process" alt="">
+		<h1 class="contact__main-title">ご相談・お問い合わせ</h1>
+		<h3>イベントや映像などについて、何でもお気軽にご相談ください。</h3>
+		<p>*入力必須項目</p>
 
-		<?php
-		if ( have_posts() ) :
+		<?php echo do_shortcode('[mwform_formkey key="58"]'); ?>
+    </div>
+</div>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main>
-
-	<div class="contact-menu">
-	
+<div class="contact-list">
+	<div class="contact-title-wrapper">
+		<p class="contact-title">お問い合わせ種別*</p>
 	</div>
+	<div class="contct-content-wrapper">[mwform_checkbox name="category-check" children="イベント・映像制作のご相談,団体・活動内容について,その他" separator="," vertically="true" class="category-check-box"]</div>
+</div>
 
-<?php
-get_footer();
+
+<div class="contact-list">
+	<div class="contact-title-wrapper">
+		<p class="contact-title">お名前 *</p>
+	</div>
+	<div class="contct-content-wrapper">[mwform_text name="name" size="" placeholder="山田花子" class="contact-box"]</div>
+</div>
+
+<div class="contact-list">
+	<div class="contact-title-wrapper">
+		<p class="contact-title">会社名・組織名</p>
+	</div>
+	<div class="contct-content-wrapper">[mwform_text name="company" size="" placeholder="株式会社xxxx" class="contact-box"]</div>
+</div>
+
+
+<div class="contact-list">
+<div class="contact-title-wrapper">
+<p class="contact-title">電話番号</p>
+
+</div>
+<div class="contct-content-wrapper">[mwform_text name="phone" size="" placeholder="09012345678" class="contact-box"]</div>
+</div>
+<div class="contact-list">
+<div class="contact-title-wrapper">
+<p class="contact-title">メールアドレス *</p>
+
+</div>
+<div class="contct-content-wrapper">[mwform_email name="email" placeholder="xxxxxx@co.jp" class="contact-box"]</div>
+</div>
+<div class="contact-list">
+<div class="contact-title-wrapper">
+<p class="contact-title">メールアドレス(確認) *</p>
+
+</div>
+<div class="contct-content-wrapper">[mwform_email name="emailcheck" placeholder="xxxxxx@co.jp" class="contact-box"]</div>
+</div>
+
+<div class="contact-list">
+<div class="contact-title-wrapper">
+<p class="contact-title">住所</p>
+</div>
+<div class="contct-content-wrapper">[mwform_text name="address" size="" placeholder="東京都 A区 B町 1-2-3" class="contact-box"]</div>
+</div>
+
+<div class="contact-list">
+<div class="contact-title-wrapper">
+<p class="contact-title">お問い合わせ内容 *</p>
+</div>
+<div class="contct-content-wrapper">[mwform_textarea name="content" cols="40" rows="10" placeholder="最大4000字まで" class="contact-content"]</div>
+</div>
+
+<p class="contact-button-wrapper">[mwform_submitButton name="confirm-submit" class="button contact-confirm-button" confirm_value="同意の上、入力内容を確認" submit_value="送信する"]
+[mwform_bback class="button contact-back-button" value="back"]修正する[/mwform_bback]</p>
+
+
+
+<?php get_footer(); 
