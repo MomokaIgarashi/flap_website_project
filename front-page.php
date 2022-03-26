fff2365072296846dcdceb3d9838ca6467c852a1
MomokaIgarashi <migarashi0318@gmail.com>
2022-03-26 20:04:12 +0100

    index news post bug fix

--- front-page.php
+++ front-page.php
@@ -65,30 +65,28 @@ get_header();
 			$the_query = new WP_Query( $args );
 		if ( $the_query->have_posts() ) :
 		?>
-		<div>
-			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-			<div class="index--news--article__wrapper">
-				<div class="index--news--article--image__wrapper">
-					<?php if ( has_post_thumbnail() ) { ?>
-						<?php the_post_thumbnail('post-thumbnail', array('class' => 'index--news--photo') ); ?>
-						<?php }else{ ?>
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
-						<?php }?>
-					<?php endwhile; ?>
-				</div>
-				<div class="index--news--article--text__wrapper">
-					<a href="<?php echo get_permalink(); ?>">
-						<div class="index--news--article--text__title">
-							<?php the_title(); ?><br>
-						</div>
-						<div class="index--news--article--text__content">
-							<?php echo wp_trim_words(get_the_content(), 60, '…もっとみる' );?><br>
-						</div>
-						<?php the_time('Y/m/d'); ?>
-					</a>
-				</div>
+		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div class="index--news--article__wrapper">
+			<div class="index--news--article--image__wrapper">
+				<?php if ( has_post_thumbnail() ) { ?>
+					<?php the_post_thumbnail('post-thumbnail', array('class' => 'index--news--photo') ); ?>
+					<?php }else{ ?>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
+					<?php }?>
+			</div>
+			<div class="index--news--article--text__wrapper">
+				<a href="<?php echo get_permalink(); ?>">
+					<div class="index--news--article--text__title">
+						<?php the_title(); ?><br>
+					</div>
+					<div class="index--news--article--text__content">
+						<?php echo wp_trim_words(get_the_content(), 60, '…もっとみる' );?><br>
+					</div>
+					<?php the_time('Y/m/d'); ?>
+				</a>
 			</div>
 		</div>
+		<?php endwhile; ?>
 		<?php endif; wp_reset_postdata(); ?>
 	</div>
 	<a href="" ><p class="index--news__link">ニュース一覧はこちら</p></a>
