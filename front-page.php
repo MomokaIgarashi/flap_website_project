92fbc70168a55535d4cbc001f938f030ad1639f1
MomokaIgarashi <migarashi0318@gmail.com>
2022-03-26 17:36:47 +0100

    (SP) Index service

--- front-page.php
+++ front-page.php
@@ -15,7 +15,7 @@ get_header();
 
 
 <div class="index--aboutus">
-	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-left.JPG" class="index--aboutus--image__left image-pc" alt="">
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-left.JPG" class="index--aboutus--image__left display-pc" alt="">
 	<div class='index--aboutus--center'>
 		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/ABOUT US.png" class="index--aboutus--image__title" alt="">
 		<h1 class="index--aboutus--center__title">わたしたちの理念</h1>
@@ -25,15 +25,25 @@ get_header();
 			<a href="" class="index--aboutus--center__link">わたしたちについて詳しくはこちら</a>
 		</p>
 	</div>
-	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-right.JPG" class="index--aboutus--image__right image-pc" alt="">
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-right.JPG" class="index--aboutus--image__right display-pc" alt="">
 	<div class="index--aboutus--image--sp__wrapper">
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-left.JPG" class="index--aboutus--image__left image-sp" alt="">
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-right.JPG" class="index--aboutus--image__right image-sp" alt="">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-left.JPG" class="index--aboutus--image__left display-sp" alt="">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/about-us-right.JPG" class="index--aboutus--image__right display-sp" alt="">
 	</div>
 </div>
 
 <div class="index--service">
-	<div class="index--service__wrapper">
+	<div class="index--service__wrapper display-sp">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/SERVICE.png" class="index--service--image__title" alt="">
+		<p class="index--service__title">サービス</p>
+		<p class="index--service__description">
+		映像を使ったイベントの提案と<br>
+		企画を実行します。様々な病気の症状、<br>
+		障がいに合わせて対応します。</p>
+	</div>
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/service-background.JPG" class="index--service--image__main display-sp" alt="">
+
+	<div class="index--service__wrapper display-pc">
 		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/SERVICE.png" class="index--service--image__title" alt="">
 		<p class="index--service__title">サービス</p>
 		<p class="index--service__description">
