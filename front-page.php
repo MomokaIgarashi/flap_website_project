49c88dfa9be4a8afb8d234c511dd80daf1487807
MomokaIgarashi <migarashi0318@gmail.com>
2022-03-27 15:51:16 +0200

    Post page PC front end

--- front-page.php
+++ front-page.php
@@ -71,7 +71,7 @@ get_header();
 				<?php if ( has_post_thumbnail() ) { ?>
 					<?php the_post_thumbnail('post-thumbnail', array('class' => 'index--news--photo') ); ?>
 					<?php }else{ ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
+					<img class="index--news--photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
 					<?php }?>
 			</div>
 			<div class="index--news--article--text__wrapper">
@@ -144,11 +144,11 @@ get_header();
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/contact-title.png" class="index--contact--image__title display-pc" alt="">
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/contact-title-sp.png" class="index--contact--image__title display-sp" alt="">
 	<div class="contact-menu--index__wrapper">
-		<a href="">
-			<button class="contact-menu__button">
+		<a class="contact-menu__button" href="<?= esc_url(home_url('/contact'));?>">
+			<!-- <button class="contact-menu__button"> -->
 					<svg class="contact-menu__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M448 64H64C28.65 64 0 92.65 0 128v256c0 35.35 28.65 64 64 64h384c35.35 0 64-28.65 64-64V128C512 92.65 483.3 64 448 64zM64 112h384c8.822 0 16 7.178 16 16v22.16l-166.8 138.1c-23.19 19.28-59.34 19.27-82.47 .0156L48 150.2V128C48 119.2 55.18 112 64 112zM448 400H64c-8.822 0-16-7.178-16-16V212.7l136.1 113.4C204.3 342.8 229.8 352 256 352s51.75-9.188 71.97-25.98L464 212.7V384C464 392.8 456.8 400 448 400z"/></svg>
 					<p class="contact-menu__button__text">お問い合わせフォーム</p>
-			</button>
+			<!-- </button> -->
 		</a>
 		<p class="contact-menu__address">問い合わせ先  mori@flap.tokyo</p>
 	</div>
--- single.php
+++ single.php
@@ -11,19 +11,24 @@ get_header();
 ?>
 
 <div class="post--wrapper">
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/NEWS.png" class="post--image__title" alt="">
 	<?php if ( has_post_thumbnail() ) { ?>
 		<?php the_post_thumbnail('post-thumbnail', array('class' => 'post--photo') ); ?>
 		<?php }else{ ?>
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
+		<img class="post--photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
 	<?php }?>
-	<div class="index--news--article--text__title">
-		<?php the_title(); ?><?php the_time('Y/m/d'); ?>
+	<div class="post--article__title">
+		<?php the_title(); ?>
 	</div>
-	<div class="index--news--article--text__content">
+	<div class="post--article__content">
 		<?php the_content( $more_link_text, $stripteaser ); ?>
 	</div>
-
-	<button>トップページに戻る</button>
+	<div class="post--article__date">
+		<?php the_time('Y/m/d'); ?>
+	</div>
+	<a class="post--article__button" href="<?= esc_url(home_url('/'));?>">
+		トップページに戻る
+	</a>
 </div>
 
 <?php
